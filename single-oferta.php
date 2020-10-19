0264e45228e81017f9150461c67ae9a0f5a7869d
Kamila <kamila.dynysiuk@gmail.com>
2020-10-19 01:19:09 +0100

    add single-oferta

--- single-oferta.php
+++ single-oferta.php
@@ -1,13 +1,33 @@
 <?php
 get_header();
-while (have_post()) {
-    the_post();
+
 ?>
-    <div class="offert-container col-10 col-md-7 col-lg-8 text-justify ">
-        <!-- <div class="offert-content"><?php the_content(); ?></div> -->
-        pojedyńcza oferta
-    </div>
+
+<main class="wrapper">
+    <section class="header-section">
+        <header class="bookmark container-fluid">
+            <h1 class="text-center">Oferta: <?php the_title(); ?></h1>
+        </header>
+    </section>
+    <section>
+        <div class="container mt-3 mb-5">
+
+            <?php
+
+            while (have_posts()) {
+                the_post();
+            ?>
+                <div class="offert-container col-10 col-md-7 col-lg-8 text-justify ">
+                    <div class="offert-content"><?php the_content(); ?></div>
+                </div>
+
+            <?php
+            }
+            ?>
+        </div>
+    </section>
+</main>
+<hr class="d-none d-lg-block">
 
 <?php
-}
 get_footer();
