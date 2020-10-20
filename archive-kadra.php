5b7d58390623d79fdd1fbd8892900eabe3e3ec23
Kamila <kamila.dynysiuk@gmail.com>
2020-10-20 14:15:20 +0100

    modyfied

--- archive-kadra.php
+++ archive-kadra.php
@@ -10,161 +10,24 @@
     </section>
 
     <section class="all-offers my-3 mx-3">
-        <!--O przedszkolu-->
         <div class="about-us column">
             <h4 class="team-text text-center py-2">Nad prawidłowym funkcjonowaniem całej placówki czuwają Panie: </h4>
             <div class="team my-1 py-1 mb-md-5 py-md-5 px-5 row justify-content-around align-items-center">
-                <div class="card col-6 col-md-4   col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Anna Kowalska</strong></p>
-                        <p class="text-center">Dyrektor placówki</p>
-                    </div>
-                </div>
-                <div class="card col-6  col-md-4  col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Maria Kowalska</strong></p>
-                        <p class="text-center">Zastępca dyrektora</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4  col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr inż. Alicja Nowak</strong></p>
-                        <p class="text-center">Administracja</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 my-1 py-1 mb-md-1 py-md-2 px-3 text-justify ">
-                <p>Kadra Niepublicznego Przedszkola „Kubusiowe Przedszkole” to zespół kreatywnych nauczycielek otwartych na
-                    potrzeby Państwa dzieci. Pracujemy według sprawdzonych programów nauczania oraz metod wychowawczych,
-                    korzystamy również z nowatorskich osiągnięć dydaktycznych aby zapewnić wszechstronny rozwój Państwa dzieci.
-                </p>
-            </div>
-            <div class="col-12 my-1 py-1 mb-md-1 py-md-2 px-3 text-justify ">
-                <h4 class="team-text text-center py-2">Ciepłą i życzliwą codzienną atmosferę zapewniają </h4>
-            </div>
 
-            <div class="team my-1 py-1 mb-md-5 py-md-5 px-5 row justify-content-around align-items-center">
-                <div class="col-12 py-3">
-                    <h3 style="color: #e52121">Grupa "Tygryski</h3>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Maria Nowak</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Anna Kowalska</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Anna Kamińska</strong></p>
-                        <p class="text-center">pomoc nauczyciela</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="team my-1 py-1 mb-md-5 py-md-5 px-5 row justify-content-around align-items-center">
-                <div class="col-12 py-3">
-                    <h3 style="color: #e52121">Grupa "Puchatki"</h3>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Ania Nowak</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Zuzanna Kowal</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Jolanta Wysocka</strong></p>
-                        <p class="text-center">nauczyciel wspomagający</p>
-                    </div>
-                </div>
             </div>
 
             <div class="team my-1 py-1 mb-md-5 py-md-5 px-5 row justify-content-around align-items-center">
                 <div class="col-12 py-3">
                     <h3 style="color: #e52121">Grupa "Kangurki"</h3>
                 </div>
-                <div class="card col-6 col-sm-5 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Hanna Kowal</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Jolanta Panna</strong></p>
-                        <p class="text-center">nauczyciel</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>mgr Ewa Nowicka</strong></p>
-                        <p class="text-center">nauczyciel wspomagający</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3  mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Klara Nowa</strong></p>
-                        <p class="text-center">pomoc nauczyciela</p>
-                    </div>
-                </div>
+
+
             </div>
             <div class="team my-1 py-1 mb-md-5 py-md-5 px-5 row justify-content-around align-items-center">
                 <div class="col-12 py-3">
                     <h3 style="color: #e52121">Obsługa</h3>
                 </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Anna Oliw</strong></p>
-                        <p class="text-center">szefowa kuchni</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Maria Kopac</strong></p>
-                        <p class="text-center">pomoc kuchenna</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profil.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Anna Jaka</strong></p>
-                        <p class="text-center">pomoc kuchenna</p>
-                    </div>
-                </div>
-                <div class="card col-6 col-md-4 col-lg-3 mb-2" style="width: 18rem;">
-                    <img src="images/profilM.png" class="card-img-top img-thumbnail" alt="kot">
-                    <div class="card-body">
-                        <p class="text-center"><strong>Patryk Nowak</strong></p>
-                        <p class="text-center">konserwtor</p>
-                    </div>
-                </div>
+
             </div>
         </div>
 
