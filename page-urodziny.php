7e8015a3a519882eb8fd2f2e3fabab5730f2ceee
Kamila <kamila.dynysiuk@gmail.com>
2020-10-20 23:07:22 +0100

    add page-urodziny

---
+++ page-urodziny.php
@@ -0,0 +1,59 @@
+<?php get_header();
+?>
+<main class="wrapper">
+    <section>
+        <header class="bookmark container-fluid">
+
+            <h1 class="text-center">Urodziny w przedszkolu</h1>
+
+        </header>
+    </section>
+    <section>
+        <section class="about-us my-4 px-3">
+            <!--Urodziny-->
+            <div class="about-us row justify-content-around ">
+                <div class="birthday about-conatiner col-10 col-md-7 col-lg-8 text-justify " style="background-image: url(<?php echo get_theme_file_uri('./images/party.png'); ?>)">
+                    <h4>Urodziny</h4>
+                    <h5>Drodzy Rodzice:</h5>
+                    <p>Każde dziecko z wielką niecierpliwością wyczekuje swoich urodzin i marzy o tym, aby był to dzień wyjątkowy.
+                        Wiemy jak wiele radości sprawia im to kiedy mogą się bawić w gronie swoich koleżanek i kolegów z przedszkola.
+                    </p><br>
+                    <h5>Nasza oferta:</h5>
+                    <p>Oferujemy możliwość zorganizowania urodzin dla dziecka w naszym przedszkolu.</p>
+                    <ul>
+                        <li><i class="fas fa-arrow-circle-right"></i> Urodziny organizujemy w godzinach popołudniowych od 15.30 do
+                            17:00 w sali i towarzystwie (wszystkich)
+                            dzieci z grupy solenizanta. Nad bezpieczeństwem dzieci oraz ciepłą i wesołą atmosferą czuwa nauczyciel oraz
+                            pomoc nauczyciela.</li>
+                        <li><i class="fas fa-arrow-circle-right"></i> Zainteresowanych prosimy zgłaszać się tydzień (minimum) przed
+                            planowaną imprezą w sekretariacie.</li>
+                        <li><i class="fas fa-arrow-circle-right"></i> Następnie podczas spotkania z nauczycielem z grupy ustalamy
+                            temat przewodni (np. Kraina Lodu, Kubuś
+                            puchatek-stary las, Minionki) oraz szczegóły.</li>
+                        <li><i class="fas fa-arrow-circle-right"></i> Nauczyciel przygotowuje na tę okazję: <br>
+                            – dekorację (stół, tablica ścienna lub inny element w zależności od sali),
+                            przeróżne atrakcje w zależności od tematyki (zabawy integracyjne np.: z chustą animacyjną, zabawy taneczne,
+                            konkurencje o charakterze sportowym i inne) <br>
+                            – Poczęstunek, zaproszenia oraz upominki dla solenizanta organizują Państwo we własnym zakresie.
+                        </li>
+                    </ul>
+                    <h5>Cennik:</h5>
+                    <p>1 wariant– urodzinową zastawę stołową (obrus, kubeczki, talerzyki, serwetki) organizuje nauczyciel- 230 zł
+                    </p>
+                    <p>2 wariant– zastawę stołową organizują Państwo we własnym zakresie (zakup i samodzielne dekorowanie stołu)
+                        –150 zł
+                        Opłata przyjmowana jest w sekretariacie przedszkola.
+                    </p>
+                    <h2 class="text-primary text-center">Serdecznie Zapraszamy!!!</h2>
+                </div>
+                <div class="up-date-kid col-12 col-md-4 col-lg-3">
+                    <div class="col-12 my-3 my-lg-4 text-center"> tutaj widget z aktualnosciami</div>
+                </div>
+            </div>
+
+
+        </section>
+</main>
+
+<?php
+get_footer();
