127754a3df4cd5ef15b48960fac2df8f9dd78f9d
Kamila <kamila.dynysiuk@gmail.com>
2020-10-22 14:10:40 +0100

    add scripts jquery

--- functions.php
+++ functions.php
@@ -4,12 +4,16 @@ function kindergarden_files()
 {
     wp_enqueue_style('bootstrap',  '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css');
     wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.7.2/css/all.css');
-    wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Pacifico&display=swap');
     wp_enqueue_script('bootstrap-js', '//code.jquery.com/jquery-3.3.1.slim.min.js', NULL, '1.0', true);
     wp_enqueue_script('bootstrap-js', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', NULL, '1.0', true);
     wp_enqueue_script('bootstrap-js', '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js', NULL, '1.0', true);
-    wp_enqueue_script('my-js', get_theme_file_uri('/js/main.js'), NULL, '1.0', true);
+    wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery-3.5.1.min.js', array('jquery'));
+
+    // wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/bundle/bundle.js', array('jquery'));
+
+    wp_enqueue_script('custom-script', get_theme_file_uri('/js/scripts.js'));
 }
 add_action('wp_enqueue_scripts', 'kindergarden_files');
 
