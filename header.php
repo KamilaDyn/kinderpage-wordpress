771a5af8be0efd6ff731af13c66bda305f92da6c
Kamila <kamila.dynysiuk@gmail.com>
2020-10-22 14:09:41 +0100

    add search loop

--- header.php
+++ header.php
@@ -12,11 +12,10 @@
 <body style="font-family: 'Lato', sans-serif;">
     <header>
 
-        <nav id="mainNavbar" class="navbar navbar-expand-lg navbar-expand-md navbar-light bg-light  ">
+        <nav id="mainNavbar" class="navbar navbar-expand-lg navbar-light bg-light  ">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
 
             <div class="collapse navbar-collapse flex-column flex-xl-row" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
@@ -48,11 +47,18 @@
                         <a class="nav-link" style="color:#ee7919" href="#">Rekrutacja</a>
                     </li>
                 </ul>
+
                 <ul class="navbar-nav mr-0 h4 text-right">
                     <li class="nav-item">
                         <a class="nav-link " style="color:#e52121" href="tel:+48123123123">+48 123-123-123</a>
                     </li>
+
                 </ul>
             </div>
+
+            <button id="search" class="search js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></button>
+
+
+
         </nav>
     </header>
