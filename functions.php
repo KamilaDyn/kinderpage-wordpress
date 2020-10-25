f98aca1fae4bea8b0a6b98e88ece6fbefa331e2d
Kamila <kamila.dynysiuk@gmail.com>
2020-10-25 01:06:32 +0100

    add path to route file

--- functions.php
+++ functions.php
@@ -1,19 +1,24 @@
 <?php
+
+
+require get_theme_file_path('/inc/search-route.php');
 /* add style and scripts to website */
 function kindergarden_files()
 {
-    wp_enqueue_style('bootstrap',  '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap',  '//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css');
     wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.7.2/css/all.css');
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Pacifico&display=swap');
-    wp_enqueue_script('bootstrap-js', '//code.jquery.com/jquery-3.3.1.slim.min.js', NULL, '1.0', true);
-    wp_enqueue_script('bootstrap-js', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', NULL, '1.0', true);
-    wp_enqueue_script('bootstrap-js', '//stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js', NULL, '1.0', true);
     wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
+    // wp_enqueue_script('bootstrap-js', '//code.jquery.com/jquery-3.3.1.slim.min.js', NULL, '1.0', true);
     wp_enqueue_script('jquery', get_stylesheet_directory_uri() . '/js/jquery-3.5.1.min.js', array('jquery'));
+    wp_enqueue_script('bootstrap-js', '//cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js');
 
-    // wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/bundle/bundle.js', array('jquery'));
+    wp_enqueue_script('custom-script', get_theme_file_uri('/js/scripts.js'),  array('jquery'));
+    // wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'));
 
-    wp_enqueue_script('custom-script', get_theme_file_uri('/js/scripts.js'));
+    wp_localize_script('custom-script', 'kindergadernData', array(
+        'root_url' => get_site_url(),
+    ));
 }
 add_action('wp_enqueue_scripts', 'kindergarden_files');
 
@@ -22,6 +27,7 @@ function kindergarden_features()
 {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('teacherSamall', 100, 70, true);
     add_image_size('teacherLand', 400, 260, true);
     add_image_size('teacherPortrait', 380, 550, true);
     add_image_size('eventsPictureSmall', 250, 150, true);
@@ -58,3 +64,15 @@ function kindergarden_adjust_queries($query)
 }
 
 add_action('pre_get_posts', 'kindergarden_adjust_queries');
+
+
+function kindergarden_custom_rest()
+{
+    register_rest_field('post', 'authorName', array(
+        'get_callback' => function () {
+            return get_the_author();
+        },
+    ));
+}
+
+add_action('rest_api_init', 'kindergarden_custom_rest');
