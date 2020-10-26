00fc2f4201e1320d57f45c5fe20c378eed6a2bb4
Kamila <kamila.dynysiuk@gmail.com>
2020-10-26 00:04:52 +0000

    add sidebar

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 
 require get_theme_file_path('/inc/search-route.php');
+
+require get_theme_file_path('/inc/widgets.php');
 /* add style and scripts to website */
 function kindergarden_files()
 {
@@ -25,6 +27,7 @@ add_action('wp_enqueue_scripts', 'kindergarden_files');
 /* add features to website*/
 function kindergarden_features()
 {
+    register_nav_menu('footerLocationOne', 'Footer Location One');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_image_size('teacherSamall', 100, 70, true);
@@ -76,3 +79,95 @@ function kindergarden_custom_rest()
 }
 
 add_action('rest_api_init', 'kindergarden_custom_rest');
+
+
+/* redirect subscriber account out of admin daschboard onto frontpage */
+function redirectSubscriberToFront()
+{
+    $currentUser = wp_get_current_user();
+    if (count($currentUser->roles) == 1 and  $currentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
+add_action('admin_init', 'redirectSubscriberToFront');
+
+
+/* hide admin bar */
+
+function noSubsAdminBar()
+{
+    $currentUser = wp_get_current_user();
+    if (count($currentUser->roles) == 1 and  $currentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
+
+
+add_action('admin_init', 'noSubsAdminBar');
+
+
+/* customize login page */
+function ourheaderurl()
+{
+    return esc_url(site_url('/'));
+}
+add_filter('login_headerurl', 'ourheaderurl');
+
+
+/*change login style */
+function ourloginCSS()
+{
+
+    wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Lato&family=Montserrat&family=Pacifico&display=swap');
+}
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+
+/*change login headline title */
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+
+/* register sidebar*/
+
+function my_register_sidebars()
+{
+    $sidebar_list = array(
+        array(
+            'name'          => __('Sidebar-oferta', 'Kindergarden'),
+            'id'            => 'offers-sidebar',
+            'description' => 'Elementy dla sidebar-1',
+        ),
+        array(
+            'name'          => __('siedebar-aktualności', 'Kindergarden'),
+            'id'            => 'events-sidebar',
+            'description' => 'Elementy dla sidebar wyświetlanego w aktualnościach',
+        ),
+        array(
+            'name'          => __('Siedebar-default', 'Kindergarden'),
+            'id'            => 'default-sidebar',
+            'description' => 'Elementy dla sidebar 3',
+        )
+    );
+
+
+    $sidebar_options = array(
+
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h4 class="widget-title">',
+        'after_title'   => '</h4>',
+    );
+    foreach ($sidebar_list as $sidebar) {
+        register_sidebar(array_merge($sidebar, $sidebar_options));
+    }
+}
+
+add_action('widgets_init', 'my_register_sidebars');
