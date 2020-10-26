c56597f5a2e8254ce969ffe80dae924f1a68049b
Kamila <kamila.dynysiuk@gmail.com>
2020-10-26 00:05:15 +0000

    modyfiy

--- js/scripts.js
+++ js/scripts.js
@@ -14,35 +14,40 @@ function getResults(){
       $.getJSON(`${kindergadernData.root_url}/wp-json/kindergarden/v1/search?term=${searchField.val()}`, results=>{
         divResults.append(`
         <div id="search-output">
-        <h3 class="py-3 text-center"> Wyniki wyszukiwania</h3>
+        <h3 class="py-3 text-center"> Wyniki wyszukiwania</wyh3>
         <div class="col-12 d-flex flex-row m-auto pt-3 justify-content-center  flex-wrap">
         <div class="col-12 col-sm-6 col-lg-4 px-2 my-3">
         <h4>Wpisy oraz strony</h4>
-        ${results.generalInfo.length ? `<ul class="list-group list-group-flush">` : `<hr class="my-0"><p class="bg-white py-2 px-4 mt-0">Brak  wyników</p>`}
-        ${results.generalInfo.map(item=> `<li class="list-group-item"><a href="${item.permalink}">${item.title}</a> ${item.postType == 'post' ? `autor: ${item.authorName}` : `` }</li>`).join('')}
+        <hr class="my-0">
+        ${results.generalInfo.length ? `<ul class="list-group list-group-flush">` : `<p class="bg-white py-2 px-4 mt-0">Brak  wyników</p>`}
+        ${results.generalInfo.map(item=> `<li class="list-group-item"><h5><a href="${item.permalink}">${item.title} ${item.postType == 'post' ? `<span>autor: ${item.authorName}</span>` : `` }</a></h5></li>`).join('')}
         ${results.generalInfo.length ? '</ul>' : ' '}
         </div>
         <div class="col-12 col-sm-6 col-lg-4 px-2 my-3 ">
         <h4>Grupy</h4>
-        ${results.grupy.length ? `<ul class="list-group list-group-flush w-100">` : `<hr class="my-0"><p class="w-100 bg-white py-3 px-4 mt-0">Brak wyników</p>`}
-        ${results.grupy.map(item=> `<li class="list-group-item"><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+        <hr class="my-0">
+        ${results.grupy.length ? `<ul class="list-group list-group-flush w-100">` : `<p class="w-100 bg-white py-3 px-4 mt-0">Brak wyników</p>`}
+        ${results.grupy.map(item=> `<li class="list-group-item"><h5><a href="${item.permalink}">${item.title}</a></h5></li>`).join('')}
         ${results.grupy.length ? '</ul>' : ' '}
         </div>
         <div class="col-12 col-sm-6 col-lg-4 px-2 my-3 ">
         <h4> Wydarzenia</h4>
-        ${results.events.length ? `<ul class="list-group list-group-flush">` : `<hr class="my-0"><p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
-        ${results.events.map(item=> `<li class="list-group-item"><a href="${item.permalink}">${item.title}. </a>Dnia: ${item.date}</li>`).join('')}
+        <hr class="my-0">
+        ${results.events.length ? `<ul class="list-group list-group-flush">` : `<p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
+        ${results.events.map(item=> `<li class="list-group-item"><h5><a href="${item.permalink}">${item.title}. </a><span class="text-black-50">Dnia: ${item.date}</span></h5></li>`).join('')}
         ${results.events.length ? '</ul>' : ' '}
         </div>
         <div class="col-12 col-sm-6 col-lg-4 px-2 my-3">
         <h4> Oferty</h4>
-        ${results.oferty.length ? `<ul class="list-group list-group-flush">` : `<hr class="my-0"><p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
-        ${results.oferty.map(item=> `<li class="list-group-item"><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+        <hr class="my-0">
+        ${results.oferty.length ? `<ul class="list-group list-group-flush">` : `<p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
+        ${results.oferty.map(item=> `<li class="list-group-item"><h5><a href="${item.permalink}">${item.title}</a></h5></li>`).join('')}
         ${results.oferty.length ? '</ul>' : ' '}
         </div>
         <div class="col-12 col-sm-6 col-lg-4 px-2 my-3">
         <h4> Kadra </h4>
-        ${results.kadra.length ? `<ul class="list-group list-group-flush">` : `<hr class="my-0"><p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
+        <hr class="my-0">
+        ${results.kadra.length ? `<ul class="list-group list-group-flush">` : `<p class="bg-white py-3 px-4 mt-0">Brak  wyników</p>`}
         ${results.kadra.map(item=> `<li class="list-group-item"><a href="${item.permalink}"><div class="d-flex justify-content-around"><h5 class="col-5">${item.title}</h5><img class="col-5" alt="zdjecie ${item.title}" src="${item.image}"></div></a>
    
         </li>`).join('')}
