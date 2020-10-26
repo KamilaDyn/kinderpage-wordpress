8ff90f2d89233c79821fcb7dc22f648228df0fdf
Kamila <kamila.dynysiuk@gmail.com>
2020-10-26 00:08:27 +0000

    add links

--- header.php
+++ header.php
@@ -1,19 +1,28 @@
 <!doctype html>
-<html lang="pl">
+<html <?php language_attributes(); ?>>
 
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
-
-    <title>Przedszkole</title>
 </head>
 
-<body>
+<body <?php body_class() ?>>
     <header>
-        <div class="d-flex justify-content-between px-3 bg-info">
-            <h5><a class="nav-link text-white" href="tel:+48123123123">+48 123-123-123</a></h5>
-            <button id="search" class="search js-search-trigger site-header__search-trigger"><i class="fa fa-search text-white" aria-hidden="true"></i></button>
+        <div class="d-sm-flex  justify-content-between px-3 bg-info ">
+            <h5><a class="nav-link text-white " href="tel:+48123123123">+48 123-123-123</a></h5>
+            <div class="login-buttons d-flex justify-content-center align-items-center mb-3 mb-sm-0 ">
+                <?php if (is_user_logged_in()) { ?>
+                    <a class="btn logout bg-success text-white mx-1" href="<?php echo wp_logout_url(); ?>">wyloguj się</a>
+                <?php } else {
+                ?>
+                    <a href="<?php echo wp_login_url(); ?>" class="btn login bg-danger text-white mx-1">zaloguj się</a>
+
+                    <a class="btn signup bg-success text-white mx-1" href="<?php echo wp_registration_url(); ?>">zarejestruj się</a>
+                <?php   }
+                ?>
+                <button id="search" class="search js-search-trigger site-header__search-trigger  "><i class="fa fa-search text-white" aria-hidden="true"></i></button>
+            </div>
         </div>
         <nav id="mainNavbar" class="navbar navbar-expand-lg  navbar-light bg-light ">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,10 +52,10 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contact">Kontakt</a>
+                        <a class="nav-link" <?php if (get_post_type() == 'kontakt') echo 'style="color: #e52121"' ?> href="<?php echo site_url('/kontakt') ?>">Kontakt</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" style="color:#ee7919" href="#">Rekrutacja</a>
+                        <a class="nav-link" style="color:#ee7919" href="<?php echo site_url('/rekrutacja') ?>">Rekrutacja</a>
                     </li>
                 </ul>
             </div>
