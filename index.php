281ef095c7f9cf6badf349a86a86c963f7f1688a
Kamila <kamila.dynysiuk@gmail.com>
2020-11-04 21:20:38 +0000

    add like-box for each post

--- index.php
+++ index.php
@@ -20,8 +20,52 @@
                     <div class="metabox bg-light">
                         <h5>Kategoria: <strong><?php the_category(', '); ?></strong> </h5>
                         <p class="text-secondary">Opublikowany przez: <?php the_author_posts_link(); ?> on <?php the_time('n.j.y') ?></p>
+
                     </div>
+
+
+
+                    <div>
+                        <?php $likeCount = new WP_Query(
+                            array(
+                                'post_type' => 'like',
+                                'meta_query' => array(
+                                    array(
+                                        'key' => 'polubione_posty_id',
+                                        'compare' => '=',
+                                        'value' => get_the_ID(),
+                                    )
+                                )
+
+                            )
+                        );
+                        $existStatus = 'no';
+                        if (is_user_logged_in()) {
+                            $existQuery = new WP_Query(array(
+                                'author' => get_current_user_id(),
+                                'post_type' => 'like',
+                                'meta_query' => array(
+                                    array(
+                                        'key' => 'polubione_posty_id',
+                                        'compare' => '=',
+                                        'value' => get_the_ID()
+                                    )
+                                )
+                            ));
+                        }
+
+                        if ($existQuery->found_posts) {
+                            $existStatus = 'yes';
+                        }
+                        ?>
+                        <span class="like-box" data-like="<?php echo $existQuery->posts[0]->ID; ?>" data-post="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
+                            <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            <span class="like-count"><?php echo  $likeCount->found_posts; ?></span>
+                        </span></div>
+
                     <p><?php the_content(); ?></p>
+
                 </article>
                 <hr class="d-none d-lg-block">
             <?php   }
